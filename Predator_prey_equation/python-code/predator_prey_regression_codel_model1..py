41972caf0fdf27ea9a6aa0b9c407dd11dcee0c9b
Sabrina Suárez <31144072+SabrinaSuarezCalcina@users.noreply.github.com>
2020-06-09 21:08:24 -0300

    Update predator_prey_regression_codel_model1..py

--- Predator_prey_equation/python-code/predator_prey_regression_codel_model1..py
+++ Predator_prey_equation/python-code/predator_prey_regression_codel_model1..py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun  9 18:20:43 2019
+Created on Tue Jun  9 18:20:43 2018
 
-@author: sabrina
+@author: Sabrina & Marcio
 """
 from sklearn.svm import NuSVR
 from sklearn.grid_search import GridSearchCV
